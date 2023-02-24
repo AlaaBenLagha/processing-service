d1dc557f1e75dc5eb193e973fec711b7c73046da
AlaaBenLagha <alaaeddine.benlagha@esprit.tn>
2023-02-24 01:08:59 +0100

    Centralized coonf + security + conversion yml to properties

--- src/main/java/com/mypfe/controller/ProcessingCheckController.java
+++ src/main/java/com/mypfe/controller/ProcessingCheckController.java
@@ -1,9 +1,7 @@
 package com.mypfe.controller;
 
 import java.util.List;
-
 import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.mypfe.model.Processing;
 import com.mypfe.serviceInterface.ProcessingCheckServiceInterface;
 
